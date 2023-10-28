4babfa24314559beb291123238e928c622f3a3b2
AndresAlba <o.alba@uniandes.edu.co>
2023-10-27 22:06:41 -0500

    Add test work experience and me

--- test/test_aspirant.py
+++ test/test_aspirant.py
@@ -44,3 +44,31 @@ class TestAspirant(TestCase):
                                     'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token)})
 
         self.assertEqual(response.status_code, 400)
+
+    @patch('src.views.view_aspirant.requests.get')
+    def test_work_experience(self, mock_get):
+
+        token = "miToken"
+
+        user_data = {
+            "id": 1,
+            "username": "UsuarioPrueba0",
+            "email": "usuarioprueba0@mail.com",
+            "type": "1"
+        }
+
+        new_work = {
+            "company": self.data_factory.company(),
+            "position": self.data_factory.job(),
+            "actualJob": False,
+            "startDate": '01/01/2010',
+            "endDate": '01/01/2012'
+        }
+
+        mock_get.return_value.json.return_value = user_data
+        mock_get.return_value.status_code = 200
+
+        response = self.client.post('/abcjobs/aspirantes/workexperience', json=new_work, headers={
+                                    'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(token)})
+
+        self.assertEqual(response.status_code, 201)
